4f4d5e9829d5dea57ce67efdee16bc14c06c238d
Adam Radocz <adam.radocz@gmail.com>
2022-01-21 23:09:05 +0100

    Update ParseCsvTests

--- FastestWaysInCSharp.Tests/FileProcessing/ParseCsv/ParseCsvTests.cs
+++ FastestWaysInCSharp.Tests/FileProcessing/ParseCsv/ParseCsvTests.cs
@@ -1,5 +1,8 @@
+using FastestWaysInCSharp.FileProcessing.ParseCsv;
 using FastestWaysInCSharp.FileProcessing.ParseCsv.V1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 
 namespace FastestWaysInCSharp.Tests.FileProcessing.ParseCsv.V1;
 
@@ -14,23 +17,23 @@ public class ParseCsvTests
     }
 
     [TestMethod]
-    public void V1_Parse100Lines_Get100Records()
-    {
-        // Act
-        var fakeNames = ReadLineSplit.Parse(_smallCsvFilePath);
-
-        // Assert
-        Assert.AreEqual(100, fakeNames.Count);
-    }
-
+    public void V1_Parse100Lines_Get100Records() => TestParsedList(ReadLineSplit.Parse(_smallCsvFilePath));
 
     [TestMethod]
-    public void V2_Parse100Lines_Get100Records()
-    {
-        // Act
-        var fakeNames = ReadLineSlice.Parse(_smallCsvFilePath);
+    public void V2_Parse100Lines_Get100Records() => TestParsedList(ReadLineSlice.Parse(_smallCsvFilePath));
 
-        // Assert
+    private static void TestParsedList(in List<FakeName> fakeNames)
+    {
         Assert.AreEqual(100, fakeNames.Count);
+        Assert.AreEqual(100, fakeNames[99].Id);
+        Assert.AreEqual(new Guid("5d1d0e33-71c6-49fe-9305-78ebc7300390"), fakeNames[99].Guid);
+        Assert.IsTrue(string.Equals("male", fakeNames[99].Gender));
+        Assert.IsTrue(string.Equals("Harrison", fakeNames[99].GivenName));
+        Assert.IsTrue(string.Equals("Lee", fakeNames[99].Surname));
+        Assert.IsTrue(string.Equals("WESTWELL", fakeNames[99].City));
+        Assert.IsTrue(string.Equals("33 Bouverie Road", fakeNames[99].StreetAddress));
+        Assert.IsTrue(string.Equals("HarrisonLee@armyspy.com", fakeNames[99].EmailAddress));
+        Assert.AreEqual(new DateOnly(1936, 5, 16), fakeNames[99].Birthday);
+        Assert.IsTrue(string.Equals("IndependentBookshop.co.uk", fakeNames[99].Domain));
     }
 }
